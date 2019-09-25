355b88fc4baae9732300221faeb4b99313e772e1
kulala2014 <125820124@qq.com>
2019-09-25 10:45:41 +0800

    Update accum_str.py

--- accum_str.py
+++ accum_str.py
@@ -1,2 +1,14 @@
 #input=‘abcde’ output=‘A-Bb-Ccc-Dddd-Eeeee’
 
+def accum(data):
+    temp_data = [(i*(data.index(i) +    1)).title() for i in data]
+    result = ‘-‘.join(temp_data)
+    return result
+
+def main():
+    testing_data = ‘abcd’
+    result = accum(testing_data)
+    print(result)
+
+if __name__ == ‘__main__’:
+    main()
